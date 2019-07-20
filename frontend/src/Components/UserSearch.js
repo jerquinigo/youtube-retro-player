6547b358be699bc4be16332ee92214157e0be90d
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-07-20 14:44:04 -0400

    working on logic to get video Id out

--- frontend/src/Components/UserSearch.js
+++ frontend/src/Components/UserSearch.js
@@ -1,12 +1,75 @@
 import React, { Component } from "react";
+import axios from "axios";
+const secret = require("../secret.json");
 
 class UserSearch extends Component {
   constructor() {
     super();
+    this.state = {
+      userInput: "",
+      videoApiData: [],
+      videoId: []
+    };
   }
 
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  getUserInput = () => {
+    return (
+      <div>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          name="userInput"
+          placeholder="enter text"
+        />
+        <button onClick={this.getVideoUrl}>submit</button>
+      </div>
+    );
+  };
+
+  getVideoUrl = () => {
+    let apikey = secret.secretkey;
+    let query = this.state.userInput;
+    // https://www.googleapis.com/youtube/v3/search?key=AIzaSyBmkkiBkyBt8LbEPsOuijYCjjJZxAPElQM&part=snippet&q=${
+    if (query) {
+      let videoIdArr = [];
+      axios
+        .get(
+          `https://www.googleapis.com/youtube/v3/search?key=${apikey}&part=snippet&q=${query}`
+        )
+        .then(response => {
+          this.setState({
+            videoApiData: response.data.items
+          });
+        })
+        .then(() => {
+          this.state.videoApiData.forEach(vid => {
+            videoIdArr.push(vid.id.videoId);
+          });
+        })
+        .then(() => {
+          this.setState({
+            videoId: videoIdArr
+          });
+        });
+    }
+  };
+
+  getVideos = () => {};
+
   render() {
-    return <div>UserSearch</div>;
+    console.log(this.state.videoId);
+    return (
+      <div>
+        UserSearch
+        {this.getUserInput()}
+      </div>
+    );
   }
 }
 
